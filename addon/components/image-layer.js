9f26ba7d16bbe4f06b591d5f1bd0c0e0927f71ca
Jan Buschtöns <buschtoens@gmail.com>
2017-03-23 18:21:48 +0100

    image-layer: reinsert, if bounds changed

--- addon/components/image-layer.js
+++ addon/components/image-layer.js
@@ -14,7 +14,23 @@ export default BaseLayer.extend({
     'url', 'opacity'
   ],
 
+  _bounds: null,
+
   createLayer() {
     return this.L.imageOverlay(...this.get('requiredOptions'), this.get('options'));
+  },
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    // if the bounds changed, remove this layer and insert it again
+    const oldBounds = this.get('_bounds');
+    const newBounds = this.get('bounds');
+    this.set('_bounds', newBounds);
+    if (oldBounds && oldBounds !== newBounds) {
+      this.willDestroyParent();
+      this.notifyPropertyChange('requiredOptions');
+      this.didInsertParent();
+    }
   }
 });
